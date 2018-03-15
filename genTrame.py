fab6714f0f5f8b278d0edd031c4197efd760cae9
Mathias Bazin <mbazin2@a-018818.ups.u-psud.fr>
2018-03-15 11:24:53 +0100

    generateur de trame version alpha experimentale

--- genTrame.py
+++ genTrame.py
@@ -1,6 +1,42 @@
 #Ca genere des trames aléatoires du format qu'on asdit qu'on fait 
+import random as r
+from datetime import datetime
 
-def genTrame(txErreur):
+#genere une trame random avecla date de maintenant
+def generateRandomTrame(txErreur=0, randomSeed = 0):
+
+	r.seed(randomSeed)
 
 	trame = "$"
+
+	trame += "h,"
+	trame += getTimeString() + ","
+
+	#tempartures en millidegre celsuic
+	trame += "t,"
+	for i in range(1, r.randint(2,5)):
+		trame += str(r.randint(5000,30000)) + ","
+		
+
+	#pressions en pascal
+	trame += "p,"
+	for i in range(1, r.randint(2,5)):
+		trame += str(r.randint(100000,100099)) + ","
 	
+	trame += "*$"
+
+	return trame
+
+	
+
+#renvoi un str de la date courante au format defini dans la trame
+def getTimeString():
+	time = str(datetime.now())
+	time = time.replace("-","")
+	time = time.replace(" ","")
+	time = time.replace(":","")
+	time = time.split(".")[0]
+	return time
+
+
+print(generateRandomTrame(0, datetime.now().microsecond))
